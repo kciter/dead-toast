d59ed47083af9c866bb07ddad443cb7cd77ac7ef
Lee Sun-Hyoup <kciter@naver.com>
2016-09-12 01:22:50 +0900

    Add cancel method

--- dead-toast.js
+++ dead-toast.js
@@ -1,4 +1,5 @@
 function DeadToast() {
+	this.bodyDOM = document.getElementsByTagName('body')[0];
 }
 
 DeadToast.makeText = function(message, duration) {
@@ -7,29 +8,47 @@ DeadToast.makeText = function(message, duration) {
 	deadToast.message = message;
 	deadToast.duration = duration;
 	return deadToast;
-}
+};
 
 DeadToast.prototype.show = function() {
-	var bodyDOM = document.getElementsByTagName('body')[0];
-	var deadToastDOM = document.createElement('div');
-	deadToastDOM.className = 'dead-toast';
-
-	var deadToastContentDOM = document.createElement('div');
-	deadToastContentDOM.className = 'dead-toast-content';
-	deadToastContentDOM.innerHTML = this.message;
-	deadToastDOM.appendChild(deadToastContentDOM);
-
-	setTimeout(function() {
+	if (this.timeoutId) {
+		clearTimeout(this.timeoutId);
+		this.timeoutId = null;
+	} else {
+		var deadToastDOM = document.createElement('div');
 		deadToastDOM.className = 'dead-toast';
-		['transitionend', 'webkitTransitionEnd', 'mozTransitionEnd', 'MSTransitionEnd'].forEach(function(transition) {
-			deadToastDOM.addEventListener(transition, function() {
-				bodyDOM.removeChild(deadToastDOM);
-			}, false);
-		})
-	}, this.duration);
+		this.deadToastDOM = deadToastDOM;
 
-	bodyDOM.appendChild(deadToastDOM);
-	setTimeout(function() {
-		deadToastDOM.className += ' show';
-	}, 10);
-}
+		var deadToastContentDOM = document.createElement('div');
+		deadToastContentDOM.className = 'dead-toast-content';
+		deadToastContentDOM.innerHTML = this.message;
+		deadToastDOM.appendChild(deadToastContentDOM);
+
+		this.bodyDOM.appendChild(deadToastDOM);
+		setTimeout(function() {
+			deadToastDOM.className += ' show';
+		}, 10);
+	}
+
+	this.timeoutId = setTimeout((function() {
+		this.cancel();
+	}).bind(this), this.duration);
+};
+
+DeadToast.prototype.cancel = function(immediately) {
+	if (this.deadToastDOM) {
+		this.deadToastDOM.className = 'dead-toast';
+		this.timeoutId = null;
+		if (immediately === true) {
+			this.bodyDOM.removeChild(this.deadToastDOM);
+			return;
+		}
+
+		var self = this;
+		['transitionend', 'webkitTransitionEnd', 'mozTransitionEnd', 'MSTransitionEnd'].forEach(function(transition) {
+			self.deadToastDOM.addEventListener(transition, function() {
+				self.bodyDOM.removeChild(self.deadToastDOM);
+			}, false);
+		});
+	}
+};
